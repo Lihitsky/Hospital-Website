d5ec12445dcae28a4773d983a64c7a9e8cd565ed
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-11-05 22:50:45 +0200

    chore: change files naming

--- react-client/src/pages/Home/Home.jsx
+++ react-client/src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
-import { AboutUs } from "../../components/Sections/about-us/AboutUs";
-import { ContactBar } from "../../components/Sections/contact-bar/ContactBar";
-import { OurServices } from "../../components/Sections/our-services/OurServices";
-import { WhyUs } from "../../components/Sections/why-us/WhyUs";
-import { BookAppointment } from "../../components/Sections/book-appointment/BookAppointment";
-import { RecentNews } from "../../components/Sections/recent-news/RecentNews";
-import { Hero } from "../../components/Sections/Hero/Hero";
+import { AboutUs } from "../../components/Sections/AboutUs";
+import { ContactBar } from "../../components/Sections/ContactBar";
+import { OurServices } from "../../components/Sections/OurServices";
+import { WhyUs } from "../../components/Sections/WhyUs";
+import { BookAppointment } from "../../components/Sections/BookAppointment";
+import { RecentNews } from "../../components/Sections/RecentNews";
+import { Hero } from "../../components/Sections/Hero";
 import { Header } from "../../components/Header";
 
 function Home() {
