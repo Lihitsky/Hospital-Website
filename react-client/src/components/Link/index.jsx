bd80207c791c6f170f229b6a0e59550d95715e34
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-11-14 21:20:31 +0200

    feat: added to Link Link from react-router-dom

--- react-client/src/components/Link/index.jsx
+++ react-client/src/components/Link/index.jsx
@@ -1,11 +1,12 @@
+import { Link as RouterLink } from "react-router-dom";
 import "./styles.scss";
 
 // Types: header, dropdown, footer, common
 
 export const Link = ({ text, href, type = "common" }) => {
   return (
-    <a className={`link ${type}`} href={href}>
+    <RouterLink className={`link ${type}`} to={href}>
       {text}
-    </a>
+    </RouterLink>
   );
 };
